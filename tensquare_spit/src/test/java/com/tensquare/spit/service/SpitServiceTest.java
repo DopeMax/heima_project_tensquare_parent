6bfaace198f403244fd9232f992e35b3edefe995
max.chen <amingccc@gmail.com>
2018-11-30 10:46:07 +0800

    mongoDB测试

---
+++ tensquare_spit/src/test/java/com/tensquare/spit/service/SpitServiceTest.java
@@ -0,0 +1,12 @@
+package com.tensquare.spit.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SpitServiceTest {
+
+    @Test
+    public void findAll() {
+    }
+}
