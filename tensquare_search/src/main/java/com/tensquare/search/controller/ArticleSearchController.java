52e0ee8cb065d0ac5540af2ee25613a13f7157ab
max.chen <amingccc@gmail.com>
2018-12-04 14:17:02 +0800

    elasticsearch，9300端口不能用的原因找到了。docker容器挂载文件位置错误

--- tensquare_search/src/main/java/com/tensquare/search/controller/ArticleSearchController.java
+++ tensquare_search/src/main/java/com/tensquare/search/controller/ArticleSearchController.java
@@ -28,7 +28,7 @@ public class ArticleSearchController {
         return new Result(true, StatusCode.OK, "操作成功");
     }
 
-    @GetMapping("/search/keywords}/{page}/{size}")
+    @GetMapping("/search/{keywords}/{page}/{size}")
     public Result findByTitleLike(@PathVariable String keywords, @PathVariable int page, @PathVariable int size) {
         Page<Article> articlePage = articleSearchService.findByTitleLike(keywords, page, size);
         return new Result(true, StatusCode.OK, "查询成功", new PageResult<Article>(articlePage.getTotalElements(), articlePage.getContent()));
